de453b18e9cc4c37ab1e2863e2c2e8fdf909e4a0
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2023-10-02 23:15:32 +0500

    fix: edit handle image logic

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,15 +1,41 @@
-import io
+import requests
+
+from datetime import datetime
 from aiogram import Router, types, F
+from aiogram.fsm.context import FSMContext
 from loader import bot
 from utils.imgtopdf import images_to_pdf
 
 router = Router()
 
 
-@router.message(F.photo)
-async def start_user(message: types.Message):
-    image_file_id = message.photo[-1].file_id
-    image = await bot.get_file(image_file_id)
-    image_path = image.file_path
-    await bot.download_file(image_path, "image.jpg")
-    images_to_pdf(["image.jpg"], "result.pdf")
+@router.message(F.content_type == 'photo')
+async def start_user(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    image_paths = data.get('image_paths', [])
+
+    for photo in message.photo:
+        now = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+        image = await bot.get_file(photo.file_id)
+        image_path = f"images/image_{now}.jpg"
+        await bot.download_file(image.file_path, image_path)
+        image_paths.append(image_path)
+
+    data['image_paths'] = image_paths
+    await state.set_data(data)
+    await message.answer("Agar rasmlar tugagan bo'lsa /stop buyrug'ini yuboring.")
+
+
+@router.message(F.text == '/stop')
+async def send_pdf(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    image_paths = data.get('image_paths', None)
+    if image_paths:
+        file_path = f'pdfs/{message.from_user.id}.pdf'
+        images_to_pdf(image_paths, file_path)
+        bot_properties = await bot.me()
+        await message.answer_document(types.input_file.FSInputFile(file_path),
+                                      caption=f"@{bot_properties.username} orqali tayyorlandi!")
+        await state.clear()
+    else:
+        await message.answer("Siz hali rasm yubormadingiz. Iltimos kamida bir dona rasm yuboring.")
--- states/test.py
+++ states/test.py
@@ -2,8 +2,7 @@ from aiogram.filters.state import StatesGroup, State
 
 
 class Test(StatesGroup):
-    Q1 = State()
-    Q2 = State()
+    get_photos = State()
 
 
 class AdminState(StatesGroup):
