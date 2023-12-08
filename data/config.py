7e5ebfd11110a48907f1dc07d9b6bae78154f307
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2023-12-08 13:30:35 +0500

    fix: just commit for test

--- data/config.py
+++ data/config.py
@@ -1,12 +1,10 @@
 from environs import Env
 
-# environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
 CHANNEL_ID = env.str("CHANNEL_ID")
 
 DB_USER = env.str("DB_USER")
